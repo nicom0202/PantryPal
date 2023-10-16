a2ae186b141a7fecbf09251c3f64e571a00f2391
Nicholas Reis <nick@eduroam-169-233-204-137.ucsc.edu>
2023-10-16 10:37:17 -0700

    updating page navigation using bottom tabs bar

---
+++ app/(tabs)/grocery.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const grocery = () => {
+    return (
+        <View>
+            <Text>Grocery</Text>
+        </View>
+    )
+}
+
+export default grocery;
--- app/index.js
+++ app/index.js
@@ -1,24 +1,6 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { Link } from "expo-router";
+import { Redirect } from "expo-router"
 
-export default function Home() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <h1>PantryPal</h1>
-
-      <Link href="/recipebook">See Recipe Book</Link>
-      <Link href="/grocery">See Grocery List</Link>
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-    root: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'whitesmoke'
-    }
-  });
+const StartPage = () => {
+    return <Redirect href="/home" />;
+};
+export default StartPage;
