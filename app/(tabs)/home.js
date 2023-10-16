126061ae1ae0f190241c37d16c03064df093373d
Nicholas Reis <nick@eduroam-169-233-204-137.ucsc.edu>
2023-10-16 11:31:33 -0700

    adding boxes for recipes

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -1,10 +1,28 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import ClickableBox from '../(components)/clickableBox';
 
 const home = () => {
+    const handleBoxClick = () => {
+        // Handle the click action here
+        alert('Box clicked!');
+      };
+
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={{ flex: 1 }}>
+            <View 
+                style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                    marginBottom: 100,
+                }}
+            >
+                <ClickableBox content="Recipe 1" onClick={handleBoxClick} />
+                <ClickableBox content="Recipe 2" onClick={handleBoxClick} />
+                <ClickableBox content={require('./chicken.jpeg')} onClick={handleBoxClick} />
+                {/* You can add more boxes here, need to figure out how to store images */}
+            </View>
         </View>
     )
 }
