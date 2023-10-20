9beaef44eca51c36638af6f584e4c26fa41bc372
FJ Tria <fjstria@gmail.com>
2023-10-20 11:57:52 -0700

    Create CheckBox component, add proto grocery list

--- CONSTANTS/theme.js
+++ CONSTANTS/theme.js
@@ -4,12 +4,12 @@
 
 const COLORS = {
     taupe: "#54494B",
-    beige: "#E2D0BE", 
+    beige: "#E2D0BE",           // default background
     darkGreen: "#082D0F",
     brightGreen: "#68A357",     // logo color
     fadedGreen: "#6F8F72",
 
-    smokeBlack: "#1A1A1A",      // default background
+    smokeBlack: "#1A1A1A",      
     lightWhite: "#FAFAFA",      // default text
 };
 
