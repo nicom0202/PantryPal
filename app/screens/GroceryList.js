2d212c162fda6e02513c98efa32975688bf04f8d
Nick Wang <nickwang@eduroam-169-233-190-213.ucsc.edu>
2023-10-25 11:04:16 -0700

    tested grocery list ingredient display

--- app/screens/GroceryList.js
+++ app/screens/GroceryList.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { View } from 'react-native';
 import CheckBox from "../../COMPONENTS/checkBox";
 import { containerStyle } from '../../STYLES/styles.js';
+import { doc, getDoc } from "firebase/firestore";
+import { db, getFirestore, collection, addDoc } from "../../firebase.js";
 
 export default function GroceryList() {
     // insert logic for generating list items here eventually
@@ -9,10 +11,28 @@ export default function GroceryList() {
     const [milk, setMilk] = useState(false);
     const [eggs, setEggs] = useState(false);
     const [bread, setBread] = useState(false);
+
+
+// third parameter needs to be pulled from the list of documents in "Recipes" collection
+    const recipeDocRef = doc(db, 'Recipes', 'cheeseburgers', 'better', 'steak');
+    getDoc(recipeDocRef)
+      .then((docSnapshot) => {
+        if (docSnapshot.exists()) {
+          const { ingredients } = docSnapshot.data();
+          console.log('Ingredients:', ingredients);
+        } else {
+          console.log('DNE');
+        }
+      })
+      .catch((error) => {
+        console.error('Error', error);
+      });
+
     
     return (
         <View style={containerStyle.container}>
             {/* again, eventually change to function for dynamic updating */}
+
             <CheckBox
                 onPress={ () => setMilk(!milk) }
                 title="Milk"
