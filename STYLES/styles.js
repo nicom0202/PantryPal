852f507db2e6be912a84e8d978ffc6f35422a1e9
Nicholas Reis <nick@eduroam-169-233-192-79.ucsc.edu>
2023-10-25 10:41:02 -0700

    new component for recipe modal, new edit mode that lets you save/delete recipe

--- STYLES/styles.js
+++ STYLES/styles.js
@@ -91,7 +91,18 @@ const buttonStyle = StyleSheet.create({
         bottom: 20, // Adjust this value to control the vertical position
         left: 113.5, // Center the button horizontally
     },
-    saveRecipeTitle: {
+    saveRecipe: {
+        width: 125,
+        height: 40,
+        borderRadius: 30,
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center', // Center the button content vertically
+        position: 'absolute', // Position the button absolutely within modalView
+        bottom: 70, // Adjust this value to control the vertical position
+        left: 113.5, // Center the button horizontally
+    },
+    editRecipe: {
         width: 125,
         height: 40,
         borderRadius: 30,
