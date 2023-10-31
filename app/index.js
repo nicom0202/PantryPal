4d697b24c1c88c8fa198bb665e7c5ffc410cdcbe
FJ Tria <fjstria@gmail.com>
2023-10-31 15:30:24 -0700

    Add icons to tab bar

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { containerStyle } from "../STYLES/styles";
 import { COLORS, SIZES } from "../CONSTANTS/theme";
 
 // SCREENS
@@ -48,12 +48,35 @@ export default function Container() {
           tabBarInactiveTintColor: COLORS.lightWhite,
           tabBarActiveBackgroundColor: COLORS.fadedGreen,
           tabBarActiveTintColor: COLORS.lightWhite,
-          }}      
+        }}
       >
 
-        <Tab.Screen name="Recipe Book" component={RecipeBook}/>
-        <Tab.Screen name="Grocery List" component={GroceryList}/>
-        <Tab.Screen name="Login" component={Login}/>
+        <Tab.Screen name="Recipe Book"
+                    component={RecipeBook}
+                    options={{
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="book-sharp" color={color} size={size} />
+                      )
+                    }}
+        />
+
+        <Tab.Screen name="Grocery List"
+                    component={GroceryList}
+                    options={{
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="list-sharp" color={color} size={size} />
+                      )
+                    }}
+        />
+
+        <Tab.Screen name="Login"
+                    component={Login}
+                    options={{
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person-sharp" color={color} size={size} />
+                      )
+                    }}
+        />
                     
       </Tab.Navigator>
 
