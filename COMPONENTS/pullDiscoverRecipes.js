3342766284dc5430945761ffea26c77aa0b34eed
nicom0202 <nicom0202@gmail.com>
2023-11-09 11:43:42 -0800

    discover page only needs add funcitonality to add recipe to RecipeBook

---
+++ COMPONENTS/pullDiscoverRecipes.js
@@ -0,0 +1,28 @@
+import { db } from "../firebase";
+import { collection, query, where, limit, getDocs } from 'firebase/firestore';
+
+async function getTenRandomRecipes(setRecipes) {
+  const randomRecipes = [];
+
+  // Fetch 10 random documents by using a random field (here, 'discoverID')
+  const q = query(collection(db, 'Discover'), where('discoverID', '>=', Math.random()), limit(3));
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach(doc => {
+    const { name, ingredients, instructions, cookTime, discoverID } = doc.data();
+    const newRecipe = {
+      id: doc.id,
+      name: name || "",
+      ingredients: ingredients || [{ name: "", quantity: "" }],
+      instructions: instructions || "",
+      cookTime: cookTime || 0,
+      discoverID: discoverID || 0
+    };
+    randomRecipes.push(newRecipe);
+  });
+
+  console.log(randomRecipes);
+  setRecipes(randomRecipes)
+}
+
+export default getTenRandomRecipes;
