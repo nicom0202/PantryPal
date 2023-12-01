b3dd7698b7ab62200e2302a4a78795a5f6a0ecdf
FJ Tria <fjstria@gmail.com>
2023-12-01 12:14:58 -0800

    Add colors and fix recipe book highlight

--- STYLES/theme.js
+++ STYLES/theme.js
@@ -2,10 +2,14 @@
 // Regulation for colors, fonts, and sizing
 
 const COLORS = {
+    brightRed: "#F8333C",
+    brightYellow: "#FCAB10",
     brightGreen: "#68A357",     // logo, tab bar, headers
-    fadedGreen: "#6F8F72",      // active tab, general accent color
-    fadedRed: "#DB5461",        // close/cancel buttons
-    fadedBlue: "#2F6690",       // select buttons
+    brightBlue: "#2B9EB3",
+    brightPurple: "#9D44B5",
+    fadedGreen: "#7EB06D",      // active tab, general accent color
+    fadedRed: "#C95654",        // close/cancel buttons
+    fadedBlue: "#3E9FE0",       // select buttons
     smokeBlack: "#1A1A1A",      // default text
     ashGray: "#8A8A8A",         // placeholder text
     lightWhite: "#FAFAFA",      // default white
