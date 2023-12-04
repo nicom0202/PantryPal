715598eb9316647e4450e4025b3908cef0343de3
nicom0202 <nicom0202@gmail.com>
2023-12-04 11:57:05 -0800

    importing constants for collection names

--- INTERFACE/PullSavedRecipes.js
+++ INTERFACE/PullSavedRecipes.js
@@ -1,8 +1,12 @@
 import { db, auth } from "../firebase";
 import { collection, query, where, getDocs } from 'firebase/firestore';
+import {
+  USER_COLLECTION_NAME,
+  RECIPES_COLLECTION_NAME
+} from "./CONSTANTS_FIREBASE";
 
 const pullSavedRecipes = (setRecipes) => {
-  const recipesCollectionRef = collection(db, 'Users', auth.currentUser.email, 'Recipes');
+  const recipesCollectionRef = collection(db, USER_COLLECTION_NAME, auth.currentUser.email, RECIPES_COLLECTION_NAME);
 
   // Creating a query to get the documents
   const q = query(recipesCollectionRef);
