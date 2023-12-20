bbf68d357f6d050ed46dc71f8c3f580c2d3ba488
Nicolas Martinez <137114121+nicom0202@users.noreply.github.com>
2023-12-20 10:05:30 -0800

    Update firebase.js
    removing key for privacy reasons.

--- firebase.js
+++ firebase.js
@@ -6,12 +6,7 @@ import { getStorage } from "firebase/storage";
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAjZ2EYVXdNVfRljDngZTdw0kzSP9qdzWY",
-    authDomain: "pantry-pal-firebase-590ad.firebaseapp.com",
-    projectId: "pantry-pal-firebase-590ad",
-    storageBucket: "pantry-pal-firebase-590ad.appspot.com",
-    messagingSenderId: "149620561014",
-    appId: "1:149620561014:web:fe8f8079995b78680ffb57"
+
 };
 
 // Initialize Firebase
